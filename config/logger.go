26aeb48440983e6fbfcae20814bf80f92322e518
Setiyono Ressly <setiyono.ressly@gmail.com>
2024-01-18 08:55:48 +0700

    add basic logger

---
+++ config/logger.go
@@ -0,0 +1,27 @@
+// log.go
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+var (
+	LoggerConfig logger.Config
+	LogFile      *os.File
+)
+
+func InitLogger() {
+	file, err := os.OpenFile("./app_logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	LoggerConfig = logger.Config{
+		Output: file,
+		// Format: "${time}|[${ip}]|${method}|${path}|${status}|${latency} ${body}\n",
+	}
+
+	LogFile = file
+}
--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
+	config.InitLogger()
+	loggerConfig := config.LoggerConfig
+	app.Use(logger.New(loggerConfig))
 	app.Use(limiter.New(config.Limiter))
 	config.Connect()
 	route.Setup(app)
